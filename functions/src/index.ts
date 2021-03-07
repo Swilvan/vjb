bae0bc8e322024fb4e2186abacef95ad6d4a660a
Silvan van Leeuwen <silvansmeel@gmail.com>
2021-03-07 14:49:12 +0100

    Question v0.1?

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const question = functions.https.onRequest((request, response) => {
+  const {name, question} = request.body;
+  const db = admin.database();
+  const questions = db.ref("questions");
+
+  questions.push({"name": name, "question": question}, (err) =>{
+    if (err == null) {
+      response.send("Question saved!");
+    } else {
+      functions.logger.error("Error saving question", err);
+      response.status(500).send();
+    }
+  });
+});
