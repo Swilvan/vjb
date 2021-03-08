45299489c95ebcaacccba1825a7f9d443cca9ac2
Silvan van Leeuwen <silvansmeel@gmail.com>
2021-03-08 19:07:04 +0100

    add cors to function and clean up front-end

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,23 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as cors from "cors";
 
 admin.initializeApp();
 
 export const question = functions.https.onRequest((request, response) => {
-  const {name, question} = request.body;
-  const db = admin.database();
-  const questions = db.ref("questions");
+  cors({origin: true})(request, response, () => {
+    functions.logger.info("handling request: ", JSON.stringify(request.body));
+    const {name, question} = request.body;
+    const db = admin.database();
+    const questions = db.ref("questions");
 
-  questions.push({"name": name, "question": question}, (err) =>{
-    if (err == null) {
-      response.send("Question saved!");
-    } else {
-      functions.logger.error("Error saving question", err);
-      response.status(500).send();
-    }
+    questions.push({"name": name, "question": question}, (err) => {
+      if (err == null) {
+        response.send("Question saved!");
+      } else {
+        functions.logger.error("Error saving question", err);
+        response.status(500).send();
+      }
+    });
   });
 });
